2b7c9c046116d0a824bcf11995a29f20fad0b001
Yash Thakare <thakareyash74@gmail.com>
2024-02-25 19:20:31 +0530

    footer/social icons/contact page added

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -22,6 +22,7 @@ router.get('/logout',async(req,res)=>{
     res.clearCookie('uid');
     return res.redirect('/login');
 })
-
-
+router.get('/contact',async(req,res)=>{
+    return res.render('contact');
+})
 module.exports = router;
