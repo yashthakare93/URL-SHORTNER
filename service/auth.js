6c4151a1f7068af23beca03cd801afa4be6696a6
Yash Thakare <thakareyash74@gmail.com>
2024-02-25 06:47:21 +0530

    jwt Authentication added

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,22 @@
-const sessionIdToUserMap = new Map();
+const JWT = require('jsonwebtoken');
+const secret = "RadheKrishna$1103003@$"
 
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user);
+function setUser(user){
+    return JWT.sign({
+        _id:user._id,
+        email:user.email
+    },secret)
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token){
+    if(!token) return null;
+    try {
+        return JWT.verify(token,secret);
+    } catch (error) {
+        return null;
+    }
+
+    
 }
 
 module.exports = {
